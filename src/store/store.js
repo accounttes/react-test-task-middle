0ecbf9cae68e31be813b5181472d5ff9da424d92
robert <smaleskirobert@gmail.com>
2025-01-12 20:19:59 +0500

    data in the cart is synchronized with localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,30 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import productsReducer from './productsSlice';
 
+const saveToLocalStorage = (store) => (next) => (action) => {
+  const result = next(action);
+  const state = store.getState();
+  localStorage.setItem('cart', JSON.stringify(state.cart));
+  return result;
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedCart = localStorage.getItem('cart');
+    if (serializedCart === null) return [];
+    return JSON.parse(serializedCart);
+  } catch (err) {
+    return [];
+  }
+};
+
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productsReducer,
   },
+  preloadedState: {
+    cart: loadFromLocalStorage(),
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saveToLocalStorage),
 });
